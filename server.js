1463b96a72b2e7c5fbc6e713e539cbf2046d236d
Chinazor Okafor <chinazorfredrick@gmail.com>
2024-08-23 10:41:41 -0400

    get endpoint modification

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.options('*', cors(corsOptions));
 
 const stripe = require("stripe")("sk_test_51PlvRgG1Zj6TIJKdXUtt0lgysWbL7KY0k4ythth1HFodYOuFhsyQYRexWdz9dZe3aPLOyXYCNazjcpfscJObXEOm00X8WFZSI8");
 
-app.get('https://stripecheckout.netlify.app/api/name', (req, res) => {
+app.get('/api/name', (req, res) => {
     res.json({ name: 'Your Name Here' });
   });
 
